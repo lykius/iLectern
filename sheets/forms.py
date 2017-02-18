695d4f246928a7c890c9a7f816510d86e0c47afa
Luca De Luigi <lucadeluigi91@gmail.com>
2017-02-18 17:51:52 +0100

    working on new sheet modal form

--- sheets/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import Sheet
-
-class SheetForm(ModelForm):
-    class Meta:
-        model = Sheet
-        fields = ['title', 'file_name']
--- sheets/urls.py
+++ sheets/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import include, url
-from .views import (SheetListView, SheetView, SheetFormView)
+from .views import SheetListView, SheetView
 
 urlpatterns = [
     url(r'^$', SheetListView.as_view(), name='list'),
-    url(r'^view/(?P<pk>[0-9]+)/$', SheetView.as_view(), name='view'),
-    url(r'^add/$', SheetFormView.as_view(), name='add')
+    url(r'^view/(?P<pk>[0-9]+)/$', SheetView.as_view(), name='view')
 ]
